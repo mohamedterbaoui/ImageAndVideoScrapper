fde3334c6f151acc8db1b25a70b61bef1bd74971
mohamed <m.terbaoui7@gmail.com>
2025-03-02 06:22:56 -0500

    Fetch webpage from url and display only images tags

--- extract.py
+++ extract.py
@@ -1,4 +1,24 @@
+import argparse
 import requests
-
 from bs4 import BeautifulSoup
 
+
+parser = argparse.ArgumentParser(description="Script pour lire les arguments")
+
+# Adding the optional arguments of the command
+
+parser.add_argument("url", help="URL of the webpage")
+
+args = parser.parse_args()
+
+# defining the url of the webpage
+url = args.url
+
+# fetching the webpage
+response = requests.get(url)
+
+# Parsing html
+soup = BeautifulSoup(response.text, "html.parser")
+
+for img in soup.find_all("img"):
+    print(img)
