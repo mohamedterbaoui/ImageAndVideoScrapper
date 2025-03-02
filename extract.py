57fb199ed9897c6ee45471ee1ff905c4ca16218a
mohamed <m.terbaoui7@gmail.com>
2025-03-02 07:09:26 -0500

    Added image option to exclude images

--- extract.py
+++ extract.py
@@ -2,13 +2,32 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 
+def extract(imageFlag, videoFlag, parsedHTML):
+    result = ""
+
+    if(imageFlag):
+        noImagesFilter = parsedHTML.find_all(lambda tag: tag.name not in ["img"])
+        noImagesFilter = parsedHTML.find_all("video")
+        for tag in noImagesFilter:
+            result+=str(tag)
+    
+    else:
+        for tag in parsedHTML.find_all(["img", "video"]):
+            result+=str(tag)+"\n"
+
+    print(result)
+
 
 parser = argparse.ArgumentParser(description="Script pour lire les arguments")
 
 # Adding the optional arguments of the command
+parser.add_argument("-i", "--image", action="store_true", help="Exclude images")
+parser.add_argument("-v", "--video", action="store_true", help="Exclude videos")
 
+# Adding required argument : URL
 parser.add_argument("url", help="URL of the webpage")
 
+# Parsing the arguments
 args = parser.parse_args()
 
 # defining the url of the webpage
@@ -20,5 +39,4 @@ response = requests.get(url)
 # Parsing html
 soup = BeautifulSoup(response.text, "html.parser")
 
-for img in soup.find_all("img"):
-    print(img)
+extract(args.image, args.video, soup)
